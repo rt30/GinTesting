47afdb5075f8bdbbdeead9eb08a246b8b0c79edb
aarti <cs18m055@smail.iitm.ac.in>
2020-08-03 17:32:01 +0530

    first

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+func main() {
+	router := SetupRouter()
+	router.Run()
+}
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "world",
+		})
+	})
+
+	return router
+}
